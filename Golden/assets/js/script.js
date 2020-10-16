d61da32b2f0a47ef5c11d089a3d0e842c8aa54cd
julian-webdev-studio <68276220+julian-webdev-studio@users.noreply.github.com>
2020-10-16 22:24:40 +0300

    Add files via upload

---
+++ Golden/assets/js/script.js
@@ -0,0 +1,10 @@
+topnav_btn.onclick = function myFunction() {
+    var x = document.getElementById('topnav');
+    if(x.className === "topnav"){
+        x.className += " responsive";
+    }
+
+    else{
+        x.className = "topnav";
+    }
+}
